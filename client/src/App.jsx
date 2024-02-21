2ac2c9d494ca3c13858c3d5913959cfc46a798ca
rahul230411 <deshmukhrd2311@gmail.com>
2024-02-21 14:43:31 +0530

    Included Differnt Routes for Different Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
 
 export default function App() {
   return (
-    <h1 className='text-slate-600'>App</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/sign-in" element={<SignIn></SignIn>}></Route>
+        <Route path="/sign-up" element={<SignUp></SignUp>}></Route>
+        <Route path="/profile" element={<Profile></Profile>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
