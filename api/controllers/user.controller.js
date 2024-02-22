9a91b0f49aa0aa82d910df399aa41d6fd99f9ce2
rahul230411 <deshmukhrd2311@gmail.com>
2024-02-22 20:49:22 +0530

    Created test API route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+export const test = (req, res) => {
+  res.json({
+    message: "JSON API is Working",
+  });
+};
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 dotenv.config();
 
 mongoose
@@ -13,6 +14,9 @@ mongoose
   });
 
 const app = express();
+
 app.listen(3000, () => {
   console.log("Server Listining on PORT 3000!!");
 });
+
+app.use("/api/user", userRoutes);
